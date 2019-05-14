1dd5aa6530dceb1cb9970129469bb7c55d2f9496
Kevin Zheng <kevzheng@Kevins-MBP-3.fios-router.home>
2019-05-14 18:08:25 -0400

    Update model tester

--- model_tester.py
+++ model_tester.py
@@ -7,6 +7,7 @@ CHANGES TO THIS FILE ARE NOT SUBMITTED.
 import time
 import subprocess
 import sys
+import multiprocessing
 
 # Change these paths to point to your local machine
 RESULT_LOCATION = '/app/data/result.txt'
@@ -14,6 +15,10 @@ DATASET_LOCATION = 'data.csv'
 SCORE_LOCATION = '/app/data/score.txt'
 
 argc = len(sys.argv)
+
+def log_pipe(pipe):
+    for line in iter(pipe.stderr.readline, b''):
+        print(line)
 
 def follow(the_process):
     while(True):
@@ -23,6 +28,9 @@ def follow(the_process):
 p = subprocess.Popen(["python3", "sample.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE) \
     if not(argc > 1 and argv[1] == "r") else \
     subprocess.Popen(["r", "-f", "sample.r", "--slave"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+stderr_logger_thread = multiprocessing.Process(target=log_pipe, args=(p,))
+stderr_logger_thread.start()
 
 output = follow(p)
 
@@ -35,13 +43,14 @@ with open(DATASET_LOCATION) as data_file, open(RESULT_LOCATION, 'a+') as result_
         data_row = data_file.readline()
         if not data_row: # EOF
             break
-        
+
         p.stdin.write(str.encode(data_row))
         p.stdin.flush()
 
-        print(p.stderr.read())
+        result_file.write(output.__next__().decode("utf-8"))
 
-        output.__next__().decode("utf-8")
+stderr_logger_thread.terminate()
 
 # Score submission
 p = subprocess.run(["python3", "scorer.py", RESULT_LOCATION, DATASET_LOCATION, SCORE_LOCATION])
+
